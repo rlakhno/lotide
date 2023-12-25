7a65ef64303ae61c2e888e9ee76d981e12aa30c8
Rouslan Lakhno <rouslanlakhno@gmail.com>
2023-12-25 14:56:59 -0500

    Removed error msg and unused consts to template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,12 +1,9 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
-  const errorMsg = (actual + " !== " + expected);
-const localActual = actual;
-const localExpected = expected;
   if (actual === expected) {
-    console.log(`👍👍👍 Assertion passed:  ${actual} === ${expected}`);
-  }else{
-    console.log(`👎👎👎 Assertion failed: ${actual} !== ${expected}`);
+    console.log(`✅✅✅ Assertion Passed:  ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
