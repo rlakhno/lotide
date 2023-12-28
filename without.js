48f9bb089a496c1690e0c51db5b8cdbda3e8cf82
Rouslan Lakhno <rouslanlakhno@gmail.com>
2023-12-27 19:38:45 -0500

    Initial commit

---
+++ without.js
@@ -0,0 +1,47 @@
+// This function checking if both arrays are equal and returns TRUE or FALSE
+function eqArrays(actualArray, expectedArray) {
+  if (actualArray.length !== expectedArray.length) {
+    return false;
+  }
+
+  for (let i = 0; i < actualArray.length; i ++) {
+    if (actualArray[i] !== expectedArray[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// Prints a message if Assertion Passed or Failed
+function assertArraysEqual(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅ ✅ ✅ Assertion Passed: ${array1}  and ${array2} are equal `);
+  } else {
+    console.log(`🛑 🛑 🛑 Assertion Failed: ${array1}  and ${array2} are NOT equal `);
+  }
+}
+
+function without(source, itemsToRemove) {
+  // Use filter to create a new array with elements not present in itemsToRemove
+  let tempArray = source.filter(function(item) {
+    return !itemsToRemove.includes(item);
+  });
+  return tempArray;
+}
+
+// Testing function - without
+const arrayOfLetters = ['a', 'b', 'c', 'd', 'e', 'f'];
+const arrayOfWithoutLetters = ['a', 'b', 'f'];
+const numbers = [1,2,3,4,5,6,7];
+const numbersWithout = [1,2,3];
+
+console.log(without([1, 2, 3], [1])); // => [2, 3]
+console.log(without([1, 2, 3], [1, 2])); // => [3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
+console.log(without(numbers, numbersWithout)); // => [4,5,6,7]
+console.log(without(arrayOfLetters, arrayOfWithoutLetters));//=>['c', 'd', 'e'];
+
+const words = ["hello", "world", "lighthouse"];
+console.log(without(words, ["lighthouse"])); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
