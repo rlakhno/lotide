5c1d30e4d3db072eaf6a2c54cd63df21ce88f178
Rouslan Lakhno <rouslanlakhno@gmail.com>
2024-01-05 15:35:53 -0500

    Initial commit

---
+++ findKey.js
@@ -0,0 +1,34 @@
+// FUNCTION assertEqual IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed:  ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+//Implementing function that returns the key when reaches the specific value
+const findKey = function(object, callback) {
+  for (let key in object) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+};
+
+//Creating a test object
+const object = {
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+};
+
+
+// TEST CODE
+assertEqual(findKey(object, x => x.stars === 2), "noma"); //=> Assertion Passed
+assertEqual(findKey(object, x => x.stars > 2), "Akaleri"); //=> Assertion Passed
+assertEqual(findKey(object, x => x.stars !== 2), "Blue Hill"); //=> Assertion Passed
+assertEqual(findKey(object, x => x.stars > 3), undefined); //=> Assertion Passed
+
