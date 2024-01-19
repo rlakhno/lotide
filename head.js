60f01d49c8e79206fdf5cc153e681ae7b2bdb4c1
Rouslan Lakhno <rouslanlakhno@gmail.com>
2024-01-19 11:31:40 -0500

    Removed export

--- head.js
+++ head.js
@@ -1,12 +1,6 @@
-
-//importing function
-const assertEqual = require('./assertEqual');
-
+// Implementing function
 const head = function(array) {
   return array[0];
 };
 
 module.exports = head;
-// // TEST CODE
-// assertEqual(head([5,6,7]), 5);
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
