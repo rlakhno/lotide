0889bd8d027808be66445c8e9402a56fdab41e40
Rouslan Lakhno <rouslanlakhno@gmail.com>
2024-01-19 13:00:34 -0500

    added exports

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,5 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed:  ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+
 
 function eqArrays(actualArray, expectedArray) {
   if (actualArray.length !== expectedArray.length) {
@@ -20,10 +14,5 @@ function eqArrays(actualArray, expectedArray) {
   return true;
 }
 
-assertEqual(eqArrays([], []), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), true); // => false
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);// => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => false
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => false
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+module.exports = eqArrays;
+
