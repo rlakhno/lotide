c1ed4dca9a8ca9c49cd1ce2c4b3702cd9f665e4a
Rouslan Lakhno <rouslanlakhno@gmail.com>
2024-01-19 13:54:09 -0500

    testing using Mochs and Chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,18 @@
 const head = require('../head');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+  
+  it("returns 5 for [5, 6, 7]", () => {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  });
+
+});
