b1b354fbd27fe7c1f75a96533fbef04fa3bb958e
Rouslan Lakhno <rouslanlakhno@gmail.com>
2024-01-19 13:03:58 -0500

    removed failed tests

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -3,9 +3,9 @@ const eqArrays = require('../eqArrays');
 
 // TESTS eqArrays
 assertEqual(eqArrays([], []), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), true); // => false
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false); // => true
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);// => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => false
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => true
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => true
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
