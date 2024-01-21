587e47d887a2b951119100339046243a33c3bb2d
Rouslan Lakhno <rouslanlakhno@gmail.com>
2024-01-21 16:01:21 -0500

    Mocha and Chai-2

--- index.js
+++ index.js
@@ -2,21 +2,39 @@
 const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
-
 const assertEqual   = require('./assertEqual');
 const assertArraysEqual   = require('./assertArraysEqual');
 const eqArrays = require('./eqArrays');
-
-
-// const map   = require('./map');
-// const flatten   = require('./flatten');
-// const without = require('./without');
+const map   = require('./map');
+const flatten   = require('./flatten');
+const without = require('./without');
+const assertObjectsEqual   = require('./assertObjectsEqual');
+const countLetters = require('./countLetters');
+const countOnly   = require('./countOnly');
+const eqObjects   = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue   = require('./findKeyByValue');
+const letterPositions   = require('./letterPositions');
+const takeUntil = require('./takeUntil');
 
 module.exports = {
+
+  assertObjectsEqual:   assertObjectsEqual,
+  countLetters:   countLetters,
+  countOnly: countOnly,
+  eqObjects:   eqObjects,
+  findKey: findKey,
+  findKeyByValue:   findKeyByValue,
+  letterPositions: letterPositions,
+  takeUntil: takeUntil,
   head:   head,
   tail:   tail,
+  middle: middle,
+  map:   map,
+  flatten:   flatten,
+  without: without,
   assertEqual: assertEqual,
   assertArraysEqual:   assertArraysEqual,
-  eqArrays: eqArrays,
-  middle: middle
+  eqArrays: eqArrays
 };
+
